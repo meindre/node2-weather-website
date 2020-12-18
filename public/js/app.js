e7733a5f6103c8963399dc98f20948c5fd4d4965
meindre01 <draebondes@gmial.com>
2020-12-18 11:09:50 +0700

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@ fetch('http://puzzle.mead.io/puzzle').then((response)=>{
     })
 })
 
-fetch('http://localhost:3000/weather?address=London').then((response)=>{
+fetch('/weather?address=London').then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             console.log(data.error)
@@ -30,7 +30,7 @@ messageTwo.textContent = ''
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
     const location = search.value
-    fetch('http://localhost:3000/weather?address=' + location).then((response)=>{
+    fetch('/weather?address=' + location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             messageOne.textContent = data.error
