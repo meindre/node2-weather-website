22b592716d9e0b6346df6564b8a3ca2107ce2425
meindre01 <draebondes@gmial.com>
2020-12-18 16:56:16 +0700

    Setup 5 app for Heroku

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude,longitude,callback)=>{
         }else if(response.body.error){
             callback('Your location not found, try another search',undefined)
         }else{
-            callback(undefined,response.body.current.weather_descriptions[0] + ' It is currently '  + response.body.current.temperature + ' Celcius degree. Feels like  ' + response.body.current.feelslike + ' degree out'
+            callback(undefined,response.body.current.weather_descriptions[0] + ' It is currently '  + response.body.current.temperature + ' Celcius degree. Feels like  ' + response.body.current.feelslike + ' degree out and humidity is ' + response.body.current.humidity 
                 
             )
         }
